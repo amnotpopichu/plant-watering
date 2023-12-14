661fbfb7dfbb36d59f874c22cbb891b1471f02e6
amnotpopichu <153849409+amnotpopichu@users.noreply.github.com>
2023-12-14 08:43:17 -0800

    Add files via upload

--- plants.py
+++ plants.py
@@ -1,9 +1,11 @@
+from datetime import UTC
 from AtlasI2C import AtlasI2C
 
 pump = AtlasI2C()
 import time
 pump.set_i2c_address(103)
-cal = 
+#2 pump
+cal = 50/43.8
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 ads = ADS.ADS1115(i2c)
@@ -14,11 +16,18 @@ value = soil_sensor.value
 #Water value = 8290, 8197, 8205
 #Bone-dry soil value = 17964, 17922, 17907
 
+
+#must add logs.txt in home directory
+
 while True:
     if  value >= 15000:
-        pump.write("D,30")
+        pump.write("D,114")
         print(value)
-        print("water dispensed")
+        f = open("logs.txt", "a")
+        f.write("Plants watered at " + str(UTC) + " with soil moisture at " + str(value))
+        f.close()
+        time.sleep(300)        
+        f.write("checking in on plants after watering, currnet soil is at " + str(value))
         time.sleep(300)
     else:
         time.sleep(300)
